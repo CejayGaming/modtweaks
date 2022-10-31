6156432dded3fc4830e3f8b98b47340f2c7e454f
CejayGaming <cejay7904@gmail.com>
2022-10-31 16:16:59 -0500

    Initial commit

---
+++ src/main/java/net/cj/modtweaks/ModTweaks.java
@@ -0,0 +1,14 @@
+package net.cj.modtweaks;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class ModTweaks implements ModInitializer {
+	public static final String MOD_ID = "modtweaks";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/cj/modtweaks/ModTweaksClient.java
@@ -0,0 +1,10 @@
+package net.cj.modtweaks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModTweaksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
